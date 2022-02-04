e377091eb15b189e438e96ec43421879addf28b7
Anderson Ventura Cerqueira <82953125+VenturaCerqueira@users.noreply.github.com>
2022-02-04 17:52:04 -0300

    Aula-01
    "Aula sobre introdução de Javascript"

---
+++ Foundation/scripts.js
@@ -0,0 +1,42 @@
+let nome = 'Rafael'; //String Literal
+let idade = 21.4; //number literal
+let number = 21; //number
+let estaAprovado = true; //Boolean
+let sobreNome; //Undefined ou let sobreNome = undefined
+let CorSelecionada = null // --> Define escolha aparti do usuario 
+
+//Tipagem Dinaminca 
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof number);
+console.log(typeof estaAprovado);
+console.log(typeof sobreNome)
+console.log(typeof CorSelecionada);
+
+//Objetos
+let user = {
+    nome:'Rafael',
+    idade:'21.4',
+    number:'21',
+}
+console.log(user);
+console.log(user.nome);
+console.log(user.idade);
+console.log(user.number);
+
+//Arrays
+
+let familia = ['Anderson', 'Adriano', 'Andressa', 'Marcelo'];
+console.log(familia);
+console.log(familia[1]);
+console.log(familia[0]);
+console.log(familia[0].length);//Anderson
+console.log(familia[1].length);//Adriano
+console.log(familia[2].length);//Andressa
+console.log(familia[3].length);//Marcelo
+console.log(typeof(familia));
+console.log(typeof(familia[1]));
+
+
+
+
